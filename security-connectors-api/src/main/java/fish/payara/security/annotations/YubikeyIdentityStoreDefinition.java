346b02d96d2cc478788e1ffa6a4f6516388f312c
Jason Tarry <jason.tarry@payara.fish>
2026-04-14 14:11:00 +0100

    FISH-12985 Deprecate YubiKey from Payara 5

--- security-connectors-api/src/main/java/fish/payara/security/annotations/YubikeyIdentityStoreDefinition.java
+++ security-connectors-api/src/main/java/fish/payara/security/annotations/YubikeyIdentityStoreDefinition.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * 
- *    Copyright (c) [2018] Payara Foundation and/or its affiliates. All rights reserved.
+ *    Copyright (c) 2018-2026 Payara Foundation and/or its affiliates. All rights reserved.
  * 
  *     The contents of this file are subject to the terms of either the GNU
  *     General Public License Version 2 only ("GPL") or the Common Development
@@ -69,6 +69,7 @@ import java.lang.annotation.Target;
 
 @Retention(value=RetentionPolicy.RUNTIME)
 @Target(value=ElementType.TYPE)
+@Deprecated
 public @interface YubikeyIdentityStoreDefinition {
     
     String YUBICO_API_KEY= "payara.security.yubikey.apikey";
